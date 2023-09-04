5a6218eaedb3b2c61f2f5efdbe78b477d2821804
athreya-harshith <athreya1207@gmail.com>
2023-09-04 19:15:05 +0530

    added some tests in index.js (this is to be removed ) and have tested some ORM inbuilt functions for associations

--- src/index.js
+++ src/index.js
@@ -4,13 +4,33 @@ const {ServerConfig,Logger} = require('./config');// no need for './config/index
 
 const app = express();
 const apiRoutes = require('./routes');
+const airport = require('./models/airport');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 //both above are for reading requests that has request body
 app.use('/api',apiRoutes);
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async ()=>{
     console.log(`Server is up and running on the port ${ServerConfig.PORT}`);
     // Logger.info({message:'some logging is begin done',error:"some error caught",label :'some label according to us'});
+    
+
+    // just for testing and its a bad practice to do this here 
+    // const {City,Airport} = require('./models');
+
+    // const bengaluru = await City.findByPk(1);
+    // console.log(bengaluru);
+    // const ariport = await Airport.create({name:'Kempegowda Intl',code:'BLR'});
+    //the above line causes error as cityId cannot be null
+    // const ariport = await bengaluru.createAirport({name:'Kempegowda Intl',code:'BLR'});
+    // do the above line only once as it gets executed every time the server starts
+    // even though we have not defined createAirport function , ORM by default provides some funcitons
+    // const hbairport = await bengaluru.createAirport({name:'Hubballi Airport',code :'HBL'});
+    // console.log(hbairport);
+    // const blrairports = await bengaluru.getAirports();
+    // console.log(blrairports);
+    // console.log(airport);
+
+    //after all the testing (temporary testing ) remove the async keyword
 });
 
