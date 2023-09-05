e9b0dadfb72c800d4af9127fc961ba6bf51c341d
athreya-harshith <athreya1207@gmail.com>
2023-09-05 10:26:24 +0530

    Created Flights model, and added Foreign key constraints using the same migrations file and corrsponding changes were done in moodels file.

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define associations here
+      this.hasMany(models.Flight,{
+        foreignKey:'airplaneId',
+        onDelete:'CASCADE'
+      });
     }
   }
   Airplane.init({
